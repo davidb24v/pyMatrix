252ea72e10e60217e46ed9ea906548c6a7763a27
Dave Boulton <davidb24v@gmail.com>
2015-01-03 10:38:51 +0000

    Add options to allow more flexibility

--- pyMatrix.py
+++ pyMatrix.py
@@ -1,24 +1,45 @@
-from __future__ import print_function
+from __future__ import print_function, division
 
 import sys
 import time
+import argparse
 
 import frames
 import matrix
 
-HOST = '192.168.0.99'
-PORT = 1234
+parser = argparse.ArgumentParser()
+parser.add_argument('img', type=argparse.FileType('r'), 
+                    help="Name of an image file (GIF, JPG, PNG etc.)")
+parser.add_argument('-b', '--brightness', type=int, default=20,
+                    help="Set brightness of display (0-255)")
+parser.add_argument('-d', '--delay', type=int, default=200,
+                    help='Delay between frames (ms)')
+parser.add_argument('-i', '--ipaddr', default='192.168.0.99',
+                    help="IP address of matrix display")
+parser.add_argument('-p', '--port', type=int, default=1234,
+                    help="TCP/IP Port number")
+args = parser.parse_args()
 
-images = frames.Frames(sys.argv[1])
+# Load file name
+images = frames.Frames(args.img)
 
-m = matrix.Matrix(HOST, PORT)
-m.setBrightness(20)
+m = matrix.Matrix(args.ipaddr, args.port)
+
+# Set brightness
+b = args.brightness
+if b < 0 or b > 255:
+    b = 20
+m.setBrightness(b)
+
+# Delay
+delay = args.delay/1000
+
 try:
     if images.length > 1:
         while True:
             for img in images.images:
                 m.send(img.tobytes())
-                time.sleep(0.2)
+                time.sleep(delay)
     else:
         m.send(images.images[0].tobytes())
         while True:
